dc2f563c7edda7405c3017700e5481d8fa924b20
Jafar Khan <profile.mdjafarkhan@gmail.com>
2026-01-31 11:51:56 +0600

    Sttep section complete mobile version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import yaml from "@rollup/plugin-yaml";
 
 export default defineConfig({
     vite: {
+        plugins: [yaml()],
         build: {
             cssCodeSplit: true, // Creates separate CSS files per import
         },
